0202387d7ddd08c10e40da3c3eb962fd53292145
rashedulalam362 <rashedulalam362@gmail.com>
2023-08-28 17:12:23 +0600

    processing

---
+++ app/api/product/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+
+export async function GET(){
+
+    return NextResponse.json({
+     
+    },{
+        status: 200,
+        headers: { 'Set-Cookie': 'theme=dark; Path=/' },
+
+
+    }
+      
+
+    )
+}
